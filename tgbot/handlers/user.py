759cbf11823eebae17d316d9dafb64cea9e97ab6
Vladimir <no.victim.no.crime451@gmail.com>
2023-04-28 10:53:54 +0300

    release v1

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,18 +1,21 @@
 from aiogram import Dispatcher
 from aiogram.dispatcher.filters import Text
 from aiogram.types import Message
-from tgbot.keyboards.reply import menu, types_task_menu
+from tgbot.keyboards.reply import menu, add_task_categories_menu, view_task_categories_menu, period_task_menu
 
 
 async def show_menu(message: Message):
     await message.answer("Что вы хотите сделать?", reply_markup=menu)
 
-async def add_task(message: Message):
-    await message.answer("к каокой категории относится эта задача?", reply_markup=types_task_menu)
+async def categories_task(message: Message):
+    if message.text == "Добавить задачу":
+        await message.answer("К какой категории относится эта задача?", reply_markup=add_task_categories_menu)
+    if message.text == "Посмотреть задачу":
+        await message.answer("К какой категории относится эта задача?", reply_markup=view_task_categories_menu)
 
 
 def register_menu(dp: Dispatcher):
     dp.register_message_handler(show_menu, commands=["start"], state="*")
 
 def register_adding_task(dp: Dispatcher):
-    dp.register_message_handler(add_task, Text(equals="Добавить задачу"), state="*")
+    dp.register_message_handler(categories_task, Text(equals=["Добавить задачу", "Посмотреть задачу"]), state="*")
--- tgbot/keyboards/reply.py
+++ tgbot/keyboards/reply.py
@@ -6,37 +6,82 @@ menu = ReplyKeyboardMarkup(
             KeyboardButton(text="Добавить задачу")
         ],
         [
-
             KeyboardButton(text="Посмотреть задачу")
         ]
     ],
     resize_keyboard=True
 )
 
-types_task_menu = ReplyKeyboardMarkup(
+add_task_categories_menu = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text="Дом")
         ],
         [
-
             KeyboardButton(text="Работа")
         ],
         [
-
             KeyboardButton(text="Учёба")
         ],
         [
-
             KeyboardButton(text="Родители")
         ],
         [
-
             KeyboardButton(text="Личная жизнь")
         ],
         [
-
             KeyboardButton(text="Друзья")
+        ]
+    ],
+    resize_keyboard=True
+)
+
+view_task_categories_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Дом")
+        ],
+        [
+            KeyboardButton(text="Работа")
+        ],
+        [
+            KeyboardButton(text="Учёба")
+        ],
+        [
+            KeyboardButton(text="Родители")
+        ],
+        [
+            KeyboardButton(text="Личная жизнь")
+        ],
+        [
+            KeyboardButton(text="Друзья")
+        ],
+        [
+            KeyboardButton(text="Посмотреть все задачи")
+        ]
+    ],
+    resize_keyboard=True
+)
+
+period_task_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Завтра")
+        ],
+        [
+            KeyboardButton(text="Послезавтра")
+        ],
+        [
+            KeyboardButton(text="Конкретная дата")
+        ],
+        [
+            KeyboardButton(text="Ближайшая неделя")
+        ],
+        [
+            KeyboardButton(text="Конкретная неделя")
+        ],
+        [
+            KeyboardButton(text="Ближайший месяц")
         ]
     ],
     resize_keyboard=True
