837c4cc1f32bf82656200726f4e3ce3f592beae3
Vladimir <no.victim.no.crime451@gmail.com>
2023-05-04 17:49:55 +0300

    keyboard

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,21 +1,63 @@
 from aiogram import Dispatcher
+from aiogram.dispatcher.filters import Text
 from aiogram.types import Message, CallbackQuery
 
-from tgbot.keyboards.inline import menu, add_task_categories_menu
+from tgbot.keyboards.inline import menu, add_task_categories_menu, view_task_categories_menu, period_task_menu
 
 
 async def show_menu(message: Message):
     await message.answer("Что вы хотите сделать?", reply_markup=menu)
 
-async def categories_task(call: CallbackQuery):
+async def back_show_menu(call: CallbackQuery):
+    await call.message.edit_text("Что вы хотите сделать?")
+    await call.message.edit_reply_markup(reply_markup=menu)
+
+async def add_categories_task(call: CallbackQuery):
     await call.message.edit_text("К какой категории относится эта задача?")
     await call.message.edit_reply_markup(reply_markup=add_task_categories_menu)
+
+async def view_categories_task(call: CallbackQuery):
+    await call.message.edit_text("К какой категории относится эта задача?")
+    await call.message.edit_reply_markup(reply_markup=view_task_categories_menu)
+
+
+async def adding_task(call: CallbackQuery):
+    await call.message.answer("Опишите вашу задачу!")
+
+async def viewing_task(call: CallbackQuery):
+    await call.message.edit_text("За какой период вы хотите посмотреть задачи?")
+    await call.message.edit_reply_markup(reply_markup=period_task_menu)
+
+
+
 
 
 def register_menu(dp: Dispatcher):
     dp.register_message_handler(show_menu, commands=["start"], state="*")
 
+def register_categories_task(dp: Dispatcher):
+    dp.register_callback_query_handler(add_categories_task, Text(startswith="add"), state="*")
+    dp.register_callback_query_handler(view_categories_task, Text(startswith="view"), state="*")
+
+
 def register_adding_task(dp: Dispatcher):
-    dp.register_callback_query_handler(categories_task, text_contains = "add", state="*")
+    dp.register_callback_query_handler(adding_task, text_contains=["add", "home"], state="*")
+    dp.register_callback_query_handler(adding_task, text_contains=["add", "work"], state="*")
+    dp.register_callback_query_handler(adding_task, text_contains=["add", "study"], state="*")
+    dp.register_callback_query_handler(adding_task, text_contains=["add", "parents"], state="*")
+    dp.register_callback_query_handler(adding_task, text_contains=["add", "friends"], state="*")
+    dp.register_callback_query_handler(adding_task, text_contains=["add", "pri_live"], state="*")
+    dp.register_callback_query_handler(back_show_menu, text_contains=["add", "back"], state="*")
+
+
+def register_viewing_task(dp: Dispatcher):
+    dp.register_callback_query_handler(viewing_task, text_contains=["view", "home"], state="*")
+    dp.register_callback_query_handler(viewing_task, text_contains=["view", "work"], state="*")
+    dp.register_callback_query_handler(viewing_task, text_contains=["view", "study"], state="*")
+    dp.register_callback_query_handler(viewing_task, text_contains=["view", "parents"], state="*")
+    dp.register_callback_query_handler(viewing_task, text_contains=["view", "friends"], state="*")
+    dp.register_callback_query_handler(viewing_task, text_contains=["view", "pri_live"], state="*")
+    dp.register_callback_query_handler(viewing_task, text_contains=["view", "etc"], state="*")
+    dp.register_callback_query_handler(back_show_menu, text_contains=["view", "back"], state="*")
 
 
--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -5,8 +5,8 @@ from tgbot.keyboards.callback_datas import *
 menu = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text="Добавить задачу", callback_data=branch_callback.new(action="add")),
-            InlineKeyboardButton(text="Посмотреть задачу", callback_data=branch_callback.new(action="view"))
+            InlineKeyboardButton(text="Добавить задачу", callback_data="add"),
+            InlineKeyboardButton(text="Посмотреть задачу", callback_data="view")
         ]
     ]
 )
@@ -29,4 +29,46 @@ add_task_categories_menu = InlineKeyboardMarkup(
             InlineKeyboardButton(text="Назад", callback_data=category_callback.new(action="add", category="back"))
         ]
     ]
+)
+
+view_task_categories_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Дом", callback_data=category_callback.new(action="view", category="home")),
+            InlineKeyboardButton(text="Работа", callback_data=category_callback.new(action="view", category="work"))
+        ],
+        [
+            InlineKeyboardButton(text="Учёба", callback_data=category_callback.new(action="view", category="study")),
+            InlineKeyboardButton(text="Родители", callback_data=category_callback.new(action="view", category="parents"))
+        ],
+        [
+            InlineKeyboardButton(text="Личная жизнь", callback_data=category_callback.new(action="view", category="pri_live")),
+            InlineKeyboardButton(text="Друзья", callback_data=category_callback.new(action="view", category="friends"))
+        ],
+        [
+            InlineKeyboardButton(text="Другое", callback_data=category_callback.new(action="view", category="etc")),
+            InlineKeyboardButton(text="Назад", callback_data=category_callback.new(action="view", category="back"))
+        ]
+    ]
+)
+
+period_task_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Завтра", callback_data=category_callback.new(action="yesday", category="back")),
+            InlineKeyboardButton(text="Послезавтра", callback_data=category_callback.new(action="aftom", category="back"))
+        ],
+        [
+            InlineKeyboardButton(text="Конкретная дата", callback_data=category_callback.new(action="date", category="back")),
+            InlineKeyboardButton(text="Ближайшая неделя", callback_data=category_callback.new(action="thisweek", category="back"))
+        ],
+        [
+            InlineKeyboardButton(text="Конкретная неделя", callback_data=category_callback.new(action="specweek", category="back")),
+            InlineKeyboardButton(text="Ближайший месяц", callback_data=category_callback.new(action="thismonth", category="back"))
+        ],
+        [
+            InlineKeyboardButton(text="Назад", callback_data=category_callback.new(action="back", category="back"))
+        ]
+    ],
+    resize_keyboard=True
 )
